c389b72de847350b05cd059b68a2f338d689e51a
cjw <charlie@destwin.com>
2019-10-13 17:13:04 -0400

    ‮suggested workaround ‫deny it exists

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ conspire = function() {
     "docker", "ubuntu", "k8s", "json", "vst", "c#", "javascript", "collectd", 
     "sleep", "satellite collections", "gyp", "lto", "macos", "smart joins",  
     "el cheapo", "v8", "salat", "windows", "transactions", "oskar", "fuerte",
-    "ccache", "libmusl", "testing.js"
+    "ccache", "libmusl", "testing.js","RTL mode"
   ];
   let conspiracy = {};
   while (Object.keys(conspiracy).length < 3) {
